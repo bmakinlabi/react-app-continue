862d346b87f9c77d4603afd7609db7e9fe71a585
Michael B. Akinlabi <35458044+MBAkinlabi@users.noreply.github.com>
2020-03-02 19:58:55 +0100

    159. Outputting Burger Ingredients Dynamically. Just a few more changes to learn more about the things done in the lecture.

--- Burger-app/my-app/src/components/Burger/Burger.js
+++ Burger-app/my-app/src/components/Burger/Burger.js
@@ -16,7 +16,7 @@ const burger = (props) => {
           {transformedIngredients}
           <BurgerIngredient type="bread-bottom" /> 
         </div>
-    );   
+    );
 };
 
 export default burger;
--- Burger-app/my-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger-app/my-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,7 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 1,
-            bacon: 2,
+            bacon: 1,
             cheese: 2,
             meat: 2
         }
