5659718c085ce8c52b0685dfe9237d03b04f01e4
Michael B. Akinlabi <35458044+MBAkinlabi@users.noreply.github.com>
2020-03-18 14:45:38 +0100

    224. Rendering Components for Routes

--- learning-http-ajax/my-app/src/axios.js
+++ learning-http-ajax/my-app/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com'
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {'Access-Control-Allow-Origin': '*'}
 });
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
