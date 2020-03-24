ef1dc240b36a30750f7197f8908a6e64c9de12b2
mbakinlabi <mbakinlabi@gmail.com>
2020-03-24 12:32:21 +0100

    263. Updating State Immutably

--- redux/src/store/reducer.js
+++ redux/src/store/reducer.js
@@ -1,24 +1,36 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            // One way to do it
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
         case 'DECREMENT':
+            // Another way to do it.
             return {
+                // We've been using the ...spread operator before nowß
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.val
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
             }
     }
     return state;
