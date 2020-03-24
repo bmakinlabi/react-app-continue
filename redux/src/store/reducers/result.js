466731598eab6ff2b3a369e10da11fb1d1a6163d
mbakinlabi <mbakinlabi@gmail.com>
2020-03-24 13:57:56 +0100

    267. Combining Multiple Reducers

---
+++ redux/src/store/reducers/result.js
@@ -0,0 +1,30 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: action.result})
+            }
+        case actionTypes.DELETE_RESULT:
+            // One way of copying the array from the object:
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1);
+
+            // Here's the second most popular way:
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }
+    }
+    return state;
+};
+
+export default reducer;
