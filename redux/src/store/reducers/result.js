5ec2e29592643b23d61a014c4f7e7e135b4e8e3e
mbakinlabi <mbakinlabi@gmail.com>
2020-03-26 14:24:44 +0100

    293. Using Utility Functions

--- redux/src/store/reducers/result.js
+++ redux/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -8,10 +9,7 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             // Change data
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result * 2})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
         case actionTypes.DELETE_RESULT:
             // One way of copying the array from the object:
             // const id = 2;
@@ -20,12 +18,10 @@ const reducer = (state = initialState, action) => {
 
             // Here's the second most popular way:
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, {results: updatedArray});
+            default:
+                return state;
     }
-    return state;
 };
 
 export default reducer;
