8fcdea3f79aea39388a39d01b788c86b02328946
mbakinlabi <mbakinlabi@gmail.com>
2020-03-30 08:50:56 +0100

    327. Adding Sign-In

--- Burger-app/my-app/src/containers/Auth/Auth.js
+++ Burger-app/my-app/src/containers/Auth/Auth.js
@@ -37,7 +37,9 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
-        }
+        },
+        isSignUp: true
+
     }
 
     checkValidity(value, rules) {
@@ -76,7 +78,13 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {isSignUp: !prevState.isSignUp};
+        });
     }
 
     render () {
@@ -105,6 +113,9 @@ class Auth extends Component {
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
+        <Button 
+            clicked={this.switchAuthModeHandler}
+            btnType="Danger">SWITCH TO {this.state.isSignUp ? 'SIGNIN' : 'SIGNUP'}</Button>
             </div>
         );
     }
@@ -112,7 +123,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     };
 }; 
 
--- Burger-app/my-app/src/store/actions/auth.js
+++ Burger-app/my-app/src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAqBXRJ1QMvsfAFam_-fVwPhK3qXiqkWm4', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAqBXRJ1QMvsfAFam_-fVwPhK3qXiqkWm4';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAqBXRJ1QMvsfAFam_-fVwPhK3qXiqkWm4';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
