89acb4d17752336bc740fdd996a57cbd4b83f531
mbakinlabi <mbakinlabi@gmail.com>
2020-04-02 17:23:17 +0100

    357. Testing Components Continued

--- Burger-app/my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ Burger-app/my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,21 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        // One way to do it:
+        // wrapper = shallow(<NavigationItems isAuthenticated />);
+        // Another way to do it:
+        wrapper.setProps({isAuthenticated: true});
+
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
