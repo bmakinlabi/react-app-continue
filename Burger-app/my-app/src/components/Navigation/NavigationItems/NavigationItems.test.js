6621406e5f20d8374586f5fd67ec66dae8f0e733
mbakinlabi <mbakinlabi@gmail.com>
2020-04-02 17:49:15 +0100

    359. Testing Components Correctly

--- Burger-app/my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ Burger-app/my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -28,7 +28,7 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
-    it('should render two <NavigationItem /> elements if not authenticated', () => {
+    it('should an exact logout button', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
