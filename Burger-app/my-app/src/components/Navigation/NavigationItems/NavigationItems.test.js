8ae52d18752f4f6522f74e6f6aa84ece7e15281a
mbakinlabi <mbakinlabi@gmail.com>
2020-04-02 17:43:18 +0100

    358. Jest and Enzyme Documentations

--- Burger-app/my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ Burger-app/my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -14,6 +14,7 @@ describe('<NavigationItems />', () => {
     beforeEach(() => {
         wrapper = shallow(<NavigationItems />);
     });
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
@@ -25,5 +26,10 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true});
 
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
