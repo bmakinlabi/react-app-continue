ad900b266734e5f49f613bda40f987bec6143bc2
mbakinlabi <mbakinlabi@gmail.com>
2020-04-03 16:44:46 +0100

    387. Setting Up a Project

---
+++ nextjs-short-course/.next/static/development/pages/Auth.js
@@ -0,0 +1,100 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/Auth.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FAuth&absolutePagePath=%2FUsers%2Fmakinlabi%2FDocuments%2FCoding%2FReact%2Freact-app-continue%2Fnextjs-short-course%2Fpages%2FAuth%2Findex.js!./":
+/*!***************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FAuth&absolutePagePath=%2FUsers%2Fmakinlabi%2FDocuments%2FCoding%2FReact%2Freact-app-continue%2Fnextjs-short-course%2Fpages%2FAuth%2Findex.js ***!
+  \***************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/Auth", function() {
+      var mod = __webpack_require__(/*! ./pages/Auth/index.js */ "./pages/Auth/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/Auth/index.js */ "./pages/Auth/index.js", function() {
+          if(!next.router.components["/Auth"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/Auth/index.js */ "./pages/Auth/index.js")
+          next.router.update("/Auth", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_2adc2403d89adc16ead0 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_2adc2403d89adc16ead0 */ "dll-reference dll_2adc2403d89adc16ead0"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/Auth/index.js":
+/*!*****************************!*\
+  !*** ./pages/Auth/index.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/makinlabi/Documents/Coding/React/react-app-continue/nextjs-short-course/pages/Auth/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var authIndexPage = function authIndexPage() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 5
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 9
+    }
+  }, "The Auth Index Page"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (authIndexPage);
+
+/***/ }),
+
+/***/ 2:
+/*!*******************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2FAuth&absolutePagePath=%2FUsers%2Fmakinlabi%2FDocuments%2FCoding%2FReact%2Freact-app-continue%2Fnextjs-short-course%2Fpages%2FAuth%2Findex.js ***!
+  \*******************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2FAuth&absolutePagePath=%2FUsers%2Fmakinlabi%2FDocuments%2FCoding%2FReact%2Freact-app-continue%2Fnextjs-short-course%2Fpages%2FAuth%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FAuth&absolutePagePath=%2FUsers%2Fmakinlabi%2FDocuments%2FCoding%2FReact%2Freact-app-continue%2Fnextjs-short-course%2Fpages%2FAuth%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_2adc2403d89adc16ead0":
+/*!*******************************************!*\
+  !*** external "dll_2adc2403d89adc16ead0" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_2adc2403d89adc16ead0;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=Auth.js.map
