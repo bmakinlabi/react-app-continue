e496db647a41e6d53f022d1aacb637ecf16fbfe3
mbakinlabi <mbakinlabi@gmail.com>
2020-04-03 10:57:31 +0100

    381. Loading Images & Injecting into HTML Page

--- webpack-short-course/webpack.config.js
+++ webpack-short-course/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -21,19 +22,33 @@ module.exports = {
                 test: /\.css$/,
                 exclude: /node_modules/,
                 use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader', options: {
-                        importLoaders: 1,
-                        modules: {
-                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader', 
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[hash:base64:5]'
+                            }
                         }
-                    }}
+                    },
+                    {
+                        loader: 'postcss-loader', 
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [autoprefixer()]
+                        } 
+                    }
                 ]
             },
-            {loader: 'postcss-loader', options: {
-                ident: 'postcss',
-                plugins: () => [autoprefixer()]
-            }}
+            { test: /\.(png|jpe?g|gif)$/, loader: 'url-loader?limit=8000&name=images/[name].[ext]' }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 };
