f357fb06e9a30c3aaa07cc8e33e7ac91d1189563
mbakinlabi <mbakinlabi@gmail.com>
2020-04-05 23:31:48 +0100

    420. Moving the Orders Side Effects into Sagas

--- Burger-app/my-app/src/store/sagas/index.js
+++ Burger-app/my-app/src/store/sagas/index.js
@@ -6,7 +6,8 @@ import {
     checkAuthTimeoutSaga, 
     authUserSaga, 
     authCheckStateSaga } from './auth';
-    import { initIngredientsSaga } from './burgerBuilder';
+import { initIngredientsSaga } from './burgerBuilder';
+import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
@@ -17,4 +18,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
