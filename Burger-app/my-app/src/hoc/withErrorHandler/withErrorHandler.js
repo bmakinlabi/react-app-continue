47de231d6b20df45d2a9589956c0aba9d081b977
mbakinlabi <mbakinlabi@gmail.com>
2020-04-07 23:50:26 +0100

    455. Converting withErrorHandler HOC

--- Burger-app/my-app/src/hoc/withErrorHandler/withErrorHandler.js
+++ Burger-app/my-app/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,65 +1,41 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-    return class extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                error: null,
-            }
-            // I'm assigning it a variable name so that I can unmount these changes.
-            /** 
-             * Also note that the instructor didn't use constructor at all. He used componentWillMount() which hass been deprecated.
-             * To get the same functionality, I'm using the constructor as recommended by the instructor. 
-            */
+    return props => {
+        const [error, setError] = useState(null);
 
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({error: null});
-                return req;
-            });
-            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-            });
-        }
+        const reqInterceptor = axios.interceptors.request.use(req => {
+            setError(null);
+            return req;
+        });
+        const resInterceptor = axios.interceptors.response.use(res => res, err => {
+            setError(err);
+        });
 
-        // I moved the code into constructor because the is showing the spinner. That's due to componentDidMount being called first. We should have used ComponentWillMount but it's been deprecated. Using the constructor is the best option.
+        useEffect(() => {
+            return () => {
+                axios.interceptors.request.eject(reqInterceptor);
+                axios.interceptors.response.eject(resInterceptor);
+            };
+        }, [reqInterceptor, resInterceptor]);
 
-        // componentDidMount() {
-        //     /*
-        //     axios.interceptors.request.use(req => {
-        //         this.setState({error: null});
-        //         return req;
-        //     })
-        //     axios.interceptors.response.use(res => res, error => {
-        //         this.setState({error: error});
-        //     });
-        //     */
-        // }
+        const errorConfirmedHandler = () => {
+            setError(null);
+        };
 
-        componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
-        }
-
-        errorConfirmedHandler = () => {
-            this.setState({error: null});
-        }
-
-        render () {
-            return (
-                <Aux>
-                    <Modal 
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}>
-                        {this.state.error ? this.state.error.message : null}
-                    </Modal>
-                    <WrappedComponent {...this.props} />
-                </Aux>
-            );
-        }
+        return (
+            <Aux>
+                <Modal 
+                    show={error}
+                    modalClosed={errorConfirmedHandler}>
+                    {error ? error.message : null}
+                </Modal>
+                <WrappedComponent {...props} />
+            </Aux>
+        );
     }
 }
 
